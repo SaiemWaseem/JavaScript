2747ab634e14fe3b6873b54071c744faed166b32
Saiem Waseem <136003524+SaiemWaseem@users.noreply.github.com>
2024-12-01 04:49:13 +0000

    Basics of the datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // this is used to enable new features of javascript or version of javascript
+
+//alert("Hello World") //we are using node.js so we can't use alert function or not using browser
+
+
+console.log(3
+   + 4) // readablity is important in programming and should be high 
+
+//console.log(3 + 4) // this is also correct but not recommended
+console.log("Sam")
+
+let name = "Sam"
+let age = 23
+let isApproved = true
+
+
+/*
+number => 2 to power 53 
+string => "" or '' but use "" because it is more readable
+boolean => true or false
+undefined => variable declared but not assigned
+null => standalone value 
+symbol => unique value
+*/
+
+
+// object 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null) // this is a bug in javascript and it will be fixed in future
+console.log(typeof undefined)
+
